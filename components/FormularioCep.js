b31e72b7490181a16fdb5017e94627d19b75727f
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-09-23 17:10:00 -0300

    botão limpar formulário

--- components/FormularioCep.js
+++ components/FormularioCep.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, FlatList} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, FlatList } from 'react-native';
 import axios from 'axios';
-import {TextInputMask} from 'react-native-masked-text';
+import { TextInputMask } from 'react-native-masked-text';
 
-const FormularioCep = ({navigation}) => {
+const FormularioCep = ({ navigation }) => {
 
     const [cep, setCep] = useState("");
     const [data, setData] = useState([]);
 
     const getData = async (cep, []) => {
         axios.get(`https://viacep.com.br/ws/${cep}/json/?callback=?`)
-            .then(res =>{
+            .then(res => {
                 const addr = res.data;
                 setData(addr)
             })
@@ -19,33 +19,38 @@ const FormularioCep = ({navigation}) => {
             });
     }
 
-return (
-    <View style={styles.container}>
-        <TextInputMask style={styles.input}
-            onChangeText={cep => { this.setCep({ cep }) }}
-            value={cep}
-            // value={'38440122'}
-            type="zip-code"
-            keyboardType='numeric'
-            onChangeText={text => setCep(text)}
-            placeholder='Digite o CEP'
-            options={{
-                maskType: 'BRL',
+    return (
+        <View style={styles.container}>
+            <TextInputMask style={styles.input}
+                onChangeText={cep => { this.setCep({ cep }) }}
+                value={cep}
+                // value={'38440122'}
+                type="zip-code"
+                keyboardType='numeric'
+                onChangeText={text => setCep(text)}
+                placeholder='Digite o CEP'
+                options={{
+                    maskType: 'BRL',
 
-            }}
-        />
-        <TouchableOpacity
-            style={styles.button}
-            onPress={() => { getData(cep, [])}}
+                }}
+            />
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => { getData(cep, []) }}
             // onPress={() => { getData(cep, []), navigation.navigate('ResultadoCep', data ) }}
-        // useEffect???
-        >
-
-            <Text style={styles.textButton}>Buscar endereço</Text>
-        </TouchableOpacity>
-        {/* <Text>{cep}</Text> */}
-        <Text>{data}</Text>
-        {/* <FlatList
+            // useEffect???
+            >
+                <Text style={styles.textButton}>Buscar endereço</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => { setCep("") }}
+            >
+                <Text style={styles.textButton}>Limpar formulário</Text>
+            </TouchableOpacity>
+            {/* <Text>{cep}</Text> */}
+            <Text>{data}</Text>
+            {/* <FlatList
           data={data}
           numColumns={1}
           keyExtractor={({index}) => index}
@@ -55,8 +60,8 @@ return (
               value={item.cep}></TextInput>
           )}
         /> */}
-    </View>
-)
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -78,10 +83,11 @@ const styles = StyleSheet.create({
     },
     button: {
         backgroundColor: '#2009f3',
-        paddingHorizontal: 20,
+        paddingHorizontal: 28,
         paddingVertical: 15,
         borderColor: '#ddd',
-        borderRadius: 5
+        borderRadius: 5,
+        marginBottom: 10
     },
     textButton: {
         color: '#ddd',
