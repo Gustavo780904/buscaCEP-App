30f088dced57a5ba39dcce960a1516c0cdb4741e
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-09-23 07:36:31 -0300

    tela inicial criada

---
+++ components/Index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, Button, StyleSheet } from 'react-native';
+
+const Sacola = ({ navigation }) => {
+    return (
+        <View>
+            <Text>teste</Text>
+            <TouchableOpacity>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        color="#fdc200"
+                        title="Busca por CEP"
+                        onPress={() => navigation.navigate('FormularioCep')}
+                    />
+                    </View>
+                    <View style={styles.buttonContainer}>
+                    <Button
+                    
+                        color="#1100fd"
+                        title="Busca por Endereço"
+                        onPress={() => navigation.navigate('FormularioRua')}
+                    />
+                </View>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+ 
+    buttonContainer: {
+        marginHorizontal: 90,
+        marginVertical: 10
+    }
+})
+
+export default Sacola;
